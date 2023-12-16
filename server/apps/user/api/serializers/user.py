c06579f2e63c4ef0b7aa501f8073609e7d3fea3b
Mazur Gleb <mag@letsnova.ru>
2023-12-16 19:37:49 +0300

    обновление логики

--- server/apps/user/api/serializers/user.py
+++ server/apps/user/api/serializers/user.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from server.apps.services.enums import UserRoleInProject
 from server.apps.services.serializers import ModelSerializerWithPermission
 
 User = get_user_model()
